ecacda57aa7b39c9a95ebbc4e40c12b9025b434c
deji-dd <dejib50@gmail.com>
2023-03-13 15:00:16 +0100

    added learn a skill section

--- school-landing-page/pages/index.tsx
+++ school-landing-page/pages/index.tsx
@@ -3,10 +3,13 @@ import MeetOurInstructors from "./MeetOurInstructors";
 import { Box } from "@mui/material";
 import Reviews from "./Reviews";
 import WBTGETL from "./WBTGETL";
+import LANSITH from "./LANSITH";
 
 export default function Index() {
   return (
     <Box sx={{ width: "80rem" }}>
+      <LANSITH />
+
       <WBTGETL />
 
       <Reviews />
